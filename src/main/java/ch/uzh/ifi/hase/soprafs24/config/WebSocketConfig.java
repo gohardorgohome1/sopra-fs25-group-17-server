61bddf1abc82f75fbc54cb7fb9f30d107bd878e8
gohardorgohome1 <gohardorgohome1@github.com>
2025-04-08 10:08:57 +0200

    basic web sockets setup #23

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package ch.uzh.ifi.hase.soprafs24.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // Enable a simple in-memory message broker to carry the message back to the client on destinations prefixed with "/topic"
+        registry.enableSimpleBroker("/topic");
+        // Define the application destination prefix
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(org.springframework.web.socket.config.annotation.StompEndpointRegistry registry) {
+        // Register the "/gs-guide-websocket" endpoint, enabling SockJS fallback options
+        registry.addEndpoint("/ws").withSockJS();
+    }
+}
