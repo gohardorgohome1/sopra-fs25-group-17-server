0e132cf434328b22c0f7e40224dc7aea02f7f336
AlexCapillaUZH <alex.capillamiralles@uzh.ch>
2025-04-09 00:03:30 +0200

    Added working support for adding and retrieving comments: #93, #94

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/rest/dto/CommentGetDTO.java
@@ -0,0 +1,35 @@
+package ch.uzh.ifi.hase.soprafs24.rest.dto;
+
+import java.time.LocalDateTime;
+
+public class CommentGetDTO {
+
+    private String userId;
+    private String message;
+    private LocalDateTime createdAt;
+
+    // Getters and Setters
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
