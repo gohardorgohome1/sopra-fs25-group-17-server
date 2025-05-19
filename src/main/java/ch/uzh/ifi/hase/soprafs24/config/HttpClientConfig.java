cea368d0da48eee9124cfac2036045a89261ec1b
PascalSenn2 <pascal.senn2@uzh.ch>
2025-05-19 19:17:37 +0200

    Added missing imports in config class

--- src/main/java/ch/uzh/ifi/hase/soprafs24/config/HttpClientConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/config/HttpClientConfig.java
@@ -1,5 +1,11 @@
 package ch.uzh.ifi.hase.soprafs24.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.http.HttpClient;
+
+
 @Configuration
 public class HttpClientConfig {
     @Bean
