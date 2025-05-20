aedfa73a6f0d42d96df75fced334c025aced6694
gohardorgohome1 <jesse.koller97@gmail.com>
2025-05-20 20:24:37 +0200

    deleted bean import

--- src/main/java/ch/uzh/ifi/hase/soprafs24/service/PhotometricCurveService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/service/PhotometricCurveService.java
@@ -6,7 +6,7 @@ import ch.uzh.ifi.hase.soprafs24.entity.DataPoint;
 import ch.uzh.ifi.hase.soprafs24.repository.PhotometricCurveRepository;
 import ch.uzh.ifi.hase.soprafs24.repository.ExoplanetRepository;
 
-import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
