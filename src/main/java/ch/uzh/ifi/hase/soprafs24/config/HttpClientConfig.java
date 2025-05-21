fc3f88ae07a6710f54ad06bbba081a84116268f4
AlexCapillaUZH <alex.capillamiralles@uzh.ch>
2025-05-21 09:34:53 +0200

    test

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/config/HttpClientConfig.java
@@ -0,0 +1,15 @@
+package ch.uzh.ifi.hase.soprafs24.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.http.HttpClient;
+
+@Configuration
+public class HttpClientConfig {
+
+    @Bean
+    public HttpClient httpClient() {
+        return HttpClient.newHttpClient();
+    }
+}
